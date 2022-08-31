0a3af62a6d5a1d5de3c80d3e2c0dbd9d0d42e783
root <root@php.anicyber.com>
2022-08-31 00:46:14 +0000

    asd

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="../assets/bootstrap.min.css" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/bootstrap.min.css" crossorigin="anonymous">
 		<title>link shrink</title>
 		<style>
 		.list-group-item:hover {
